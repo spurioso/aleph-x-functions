8b1e96527ac673e8242e1155dcb7d2d22276f004
spurioso <spurioso@gmail.com>
2013-09-22 11:17:21 -0400

    AlephX object now has marc method
    * Returns marc bib record with marc method
    * Added functions needed to get to this point:
    	* alephXfind()
    	* alephXgetSetNum()
    	* buildPresentURL()
    	* alephXpresent()

--- alephXobjects.php
+++ alephXobjects.php
@@ -24,7 +24,10 @@ class AlephX {
 			} // end if
 		} // end foreach
 		$this->findURL = $this->buildFindURL($hostname, $path, $request, $code, $base);	
-		
+		$this->findXML = $this->alephXfind($this->findURL);
+		$setNum = $this->alephXgetSetNum($this->findXML);
+		$this->presentURL = $this->buildPresentURL($hostname, $path, $setNum);
+		$this->marc = $this->alephXpresent($this->presentURL);
 	} // end __construct
 		
 	// Build a url for use in the alephXfind() function. Requires a $request and a $code (i.e. "BAR")  
@@ -33,29 +36,35 @@ class AlephX {
 		return($findURL);
 	} // end buildFindURL
 	
+	// Returns XML about a set of resuts. Requires the $findURL from buildFindURL()
+	protected function alephXfind($findURL) {	
+		$findResults = file_get_contents($findURL); // get results of find request
+		$findXML = new SimpleXMLElement($findResults); // turn the results into an XML object
+		return($findXML);	
+	} // end alephXfind
+	
+	// Returns set number for a result set. Requires $findXML returned from alephXfind()
+	protected function alephXgetSetNum($findXML) {			
+		$setNum = $findXML->set_number;
+		return($setNum);	
+	} // end alephXgetSetNum
+		
+	// Build a url for use in the alephXpresent() function. Requires the $setNum returned by alephXgetSetNum()
+	protected function buildPresentURL($hostname, $path, $setNum, $setEntry = 1) {
+		$presentURL = $hostname.$path."set_no=".$setNum."&set_entry=".$setEntry."&op=present";
+		return($presentURL);
+	} // end buildPresentURL
+	
+	// Returns MarcXML for a record. Default is the first result of a set. Requires the $findURL from alephXbuildFindURL
+	function alephXpresent($presentURL, $setEntry = 1){	
+		$presentResults = file_get_contents($presentURL);
+		$presentXML = new SimpleXMLElement($presentResults);
+		return($presentXML); 
+} // end alephPresent	
 	
 } // end AlephX object
 
 $book = new AlephX("31430045584994", "barcode");
-echo $book->findURL;
-
-// Build a url for use in the alephXfind() function. Requires a $request and a $code (i.e. "BAR")  
-function alephXbuildFindURL($request, $code, $base = "CP", $op = "find") {
-	$findURL = "http://catalog.umd.edu/X?request=".$request."&op=".$op."&code=".$code."&base=".$base;
-	return($findURL);
-}
-
-// Build a url for use in the alephXpresent() function. Requires the $setNum returned by alephXgetSetNum()
-function alephXbuildPresentURL($setNum, $setEntry = 1) {
-	$presentURL = "http://catalog.umd.edu/X?set_no=".$setNum."&set_entry=".$setEntry."&op=present";
-	return($presentURL);
-}
-
-// Returns XML about a set of resuts. Requires the $findURL from alephXbuildFindURL
-function alephXfind($findURL) {	
-	$findResults = file_get_contents($findURL); // get results of find request
-	$findXML = new SimpleXMLElement($findResults); // turn the results into an XML object
-	return($findXML);	
-} // end alephFind
+print_r($book->marc);
 
 ?>
